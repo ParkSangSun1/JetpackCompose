508e8c5452e10f37f9dc2e1a1e9a4b20257d7575
ParkSangSun <appleasz1028102@gmail.com>
2021-09-09 09:09:36 +0900

    modifier 및 기타 매개변수

--- app/src/main/java/com/study/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/study/jetpackcompose/MainActivity.kt
@@ -3,36 +3,64 @@ package com.study.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.study.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
-                }
-            }
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!",
+        //텍스트 사이즈
+        fontSize = 20.sp,
+        //텍스트 굵기
+        fontWeight = FontWeight.Bold,
+        //텍스트 컬러
+        color = Color.Blue,
+        //텍스트 정렬
+        textAlign = TextAlign.Center,
+
+        modifier = Modifier
+            .background(color = Color.Yellow)
+            //테두리
+            .border(2.dp, color = Color.Green)
+            .padding(10.dp)
+            //width 길이를 부모 전체로 설정
+            //0.5f는 반을 의미 1.0f가 1(전체)
+            .fillMaxWidth(0.5f)
+            //height 길이를 부모 전체로 설정
+            .fillMaxHeight(0.3f)
+        )
 }
 
+//안드로이드 스튜디오에서 프리뷰를 보기위한 주석
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
-        Greeting("Android")
+        Greeting("Yeoooo")
     }
 }
