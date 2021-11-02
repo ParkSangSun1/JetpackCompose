885885e9fcb795a88f7de04ab615912af5a00e0b
ParkSangSun <appleasz1028102@gmail.com>
2021-11-02 12:17:23 +0900

    공식문서 보고 공부하기(머티리얼 디자인)

--- app/src/main/java/com/study/jetpackcompose/MainActivity2.kt
+++ app/src/main/java/com/study/jetpackcompose/MainActivity2.kt
@@ -1,9 +1,11 @@
 package com.study.jetpackcompose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.shape.CircleShape
@@ -14,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.study.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -29,27 +32,43 @@ class MainActivity2 : ComponentActivity() {
 
 data class Message(val author: String, val body: String)
 
+
 @Composable
 fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)){
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
             contentDescription = "Study Ex",
             modifier = Modifier
                 .size(60.dp)
                 .clip(CircleShape)
+                .border(
+                    1.5.dp, MaterialTheme.colors.secondary, CircleShape
+                )
         )
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp){
+                Text(text = msg.body)
+            }
         }
     }
 }
 
 @Preview
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
     MessageCard(
